cb7cba9c1c62e8392c390b8ce7b45442dce4f7cc
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-11-19 21:07:00 +0000

    Add GameEvent and GameEventListener
    Pattern based on Ryan Hipple's Unite 2017 talk - Game Architecture with Scriptable Objects

---
+++ Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,18 @@
+﻿using CarConfigurator.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CarConfigurator
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent @event;
+        public UnityEvent response;
+
+        private void OnEnable() => @event.RegisterListener(this);
+
+        private void OnDisable() => @event.UnregisterListener(this);
+
+        public void OnEventRaised() => response.Invoke();
+    }
+}
