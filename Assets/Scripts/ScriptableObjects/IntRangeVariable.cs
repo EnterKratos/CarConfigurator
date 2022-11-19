bd33c7c5d7e48c540341c513328cb33c6b605d30
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-11-19 20:34:00 +0000

    Add IntRangeVariable scriptable object

---
+++ Assets/Scripts/ScriptableObjects/IntRangeVariable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace CarConfigurator.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Variables/Int (Range)")]
+    public class IntRangeVariable : ScriptableObject
+    {
+        public int defaultValue = 0;
+        public int min;
+        public int max;
+        public bool loopRange = false;
+        public int value;
+
+        public void Increment()
+        {
+            if (loopRange)
+            {
+                if (value + 1 > max)
+                {
+                    value = min;
+                    return;
+                }
+            }
+
+            if (value < max)
+            {
+                value++;
+            }
+        }
+
+        public void Decrement()
+        {
+            if (loopRange)
+            {
+                if (value - 1 < min)
+                {
+                    value = max;
+                    return;
+                }
+            }
+
+            if (value > min)
+            {
+                value--;
+            }
+        }
+
+        private void OnEnable()
+        {
+            value = defaultValue;
+        }
+    }
+}
