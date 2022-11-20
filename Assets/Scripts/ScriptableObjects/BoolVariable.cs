15801b14f77a0690e5dead2dba8d9f6d3ed519c7
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-11-20 14:28:47 +0000

    Add stub AddFeatures modal

---
+++ Assets/Scripts/ScriptableObjects/BoolVariable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CarConfigurator.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Variables/Bool")]
+    public class BoolVariable : ScriptableObject
+    {
+        public bool value;
+
+        public void Set() => value = true;
+        public void Unset() => value = false;
+    }
+}
